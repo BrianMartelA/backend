519bf65cbee876815ad2c8bd811ff3ae9c0a65a8
BrianMartelA <brianmartelabdala@gmail.com>
2025-06-16 21:23:30 -0400

    actualización

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User  # importa tu modelo personalizado
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'rut')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'rut')
